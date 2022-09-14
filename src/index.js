71a0eec6066f681a9e0973f2afe50d7c49e524de
wooldridge <mike@hellion.org>
2022-09-14 14:51:01 -0700

    Minimize CRA starter code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ReactiveBase, DataSearch } from "@appbaseio/reactivesearch";
+import appConfig from './config/app.config';
+import searchConfig from './config/search.config';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ResultsList } from 'maftest-button';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+
+    const name = appConfig.name || "Search App";
+
+    return (
+        <ReactiveBase
+            url="https://appbase-demo-ansible-abxiydt-arc.searchbase.io"
+            app="good-books-ds"
+            credentials="04717bb076f7:be54685e-db84-4243-975b-5b32ee241d31"
+            enableAppbase
+        >
+        <div>
+            <header style={{backgroundColor: "rgb(43, 51, 60)", width: "100%", padding: "10px"}}>
+            <h1 style={{fontSize: "18px", color: "#CCC", fontWeight: "bold"}}>{name}</h1>
+            {/* <SearchBox config={searchboxConfig.searchbox} button="horizontal" handleSearch={handleSearch} width="600px" /> */}
+            </header>
+            <div style={{padding: '10px'}}>
+                {/* <ResultsList 
+                data={result} 
+                config={ api === "node" ? 
+                    searchNodeConfig.search.results.config : 
+                    searchConfig.search.results.config } 
+                /> */}
+            </div>
+        </div>
+        </ReactiveBase>
+    )
+}
+
+root.render(<App />);
